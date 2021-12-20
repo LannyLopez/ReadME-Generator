1cdb4f7e56652425bbf70403ba4a01c9111b07c9
lanny lopez <lannylynnlopez89@gmail.com>
2021-12-19 23:08:56 -0700

    added all the questions

--- Develop/index.js
+++ Develop/index.js
@@ -12,12 +12,61 @@ const questions = () => {
             if (nameInput){
                 return true;
             } else {
-                console.log('please enter your name! ')
+                console.log('please enter your project name! ')
                 return false
             }
         }
-        
-    }])
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a Description of your project (required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+            return true
+            }
+            else {
+                console.log('You need to input a description of project! ')
+                return false
+            }
+        }
+
+    },
+    {
+        type: 'checkbox',
+        name: 'Languages',
+        message: 'What did you use create the app with ',
+        choices: ['HTML', 'CSS', 'Javascript', 'Node', 'React', 'Java', 'Python',]
+    },
+    {
+        type: 'input',
+        name: 'Github',
+        message: 'Please provide a Link to your github account! (required)',
+        validate: linkInput => {
+            if (linkInput) {
+                return true
+            } else {
+                console.log ('Please provide a link to your GitHub account! ')
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'Please provide your email! '
+    },
+    {
+        type: 'checkbox',
+        name: 'Licenses',
+        message: 'What type of License will you be using',
+        choices: ['MIT','GNU AGPLv3', 'Apache License 2.0','The Unlicense']
+    }
+      
+
+    
+
+])
 }
 
 // TODO: Create a function to write README file
