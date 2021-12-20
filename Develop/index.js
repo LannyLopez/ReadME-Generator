16dfca4741d236e68c8fc03ca76ef7e159fb7847
lanny lopez <lannylynnlopez89@gmail.com>
2021-12-19 20:36:01 -0700

    added ability to ask question

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,32 @@
 // TODO: Include packages needed for this application
 const inquirer = require ('inquirer')
 const generateMarkdown = require ('./utils/generateMarkdown')
+const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title? (required) ',
+        validate: nameInput => {
+            if (nameInput){
+                return true;
+            } else {
+                console.log('please enter your name! ')
+                return false
+            }
+        }
+        
+    }])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return questions()
+}
 
 // Function call to initialize app
 init();
